7bde4fa54fede3b72b1e2becaacef47cd41628be
juancruzsb <sabiojuancruzsecundario@gmail.com>
2025-08-12 12:01:39 -0300

    termine creo

--- dbconfig.js
+++ dbconfig.js
@@ -5,7 +5,7 @@
 
 
 export const config = {
-    host :process.env.PGHOST,
+    host:process.env.PGHOST,
     database:process.env.PGDATABASE,
     user:process.env.PGUSER,
     password:process.env.PGPASSWORD,
